9f740db4977ce51ae5ad39744be767a798990797
pokehelper <60897975+pokehelper@users.noreply.github.com>
2020-02-14 19:36:16 +0000

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+jQuery(document).ready(function() {
+	jQuery(".eTimer").eTimer({
+		etType: 0, 
+		etDate: "22.02.2020.11.0", 
+		etTitleSize: 0, 
+		etShowSign: 0, 
+		etSep: ":", 
+		etFontFamily: "Roboto", 
+		etTextColor: "#44696c", 
+		etPaddingTb: 10,
+		etPaddingLR: 10, 
+		etBackground: "white", 
+		etBorderSize: 0, 
+		etBorderRadius: 2, 
+		etBorderColor: "white", 
+		etLastUnit: 3, 
+		etNumberFontFamily: "Roboto", 
+		etNumberSize: 35, 
+		etNumberColor: "#44696c", 
+		etNumberPaddingTB: 0, 
+		etNumberPaddingLR: 8, 
+		etNumberBackground: "white", 
+		etNumberBorderSize: 0, 
+		etNumberBorderRadius: 0, 
+		etNumberBorderColor: "#040303", 
+	});
+});
